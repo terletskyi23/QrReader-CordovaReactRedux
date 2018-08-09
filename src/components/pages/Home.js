31f645cc7199d920ef3f398f9e7880b335066e3f
yurkagon <owngameplay@gmail.com>
2018-08-09 19:11:34 +0300

    refactor

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,43 +2,24 @@ import React, { Component } from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import { Link } from 'react-router-dom';
 
-function displayContents(err, text){
-  if(err) {
-    alert(JSON.stringify(err));
-  } else {
-    // The scan completed, display the contents of the QR code:
-    alert(text);
-  }
-}
+const Home = () => (
+  <div className="home-page">
+    <div className="center-block">
+      <h2>QR</h2>
 
-class Home extends Component {
-  componentDidMount() {
-    QRScanner.scan(displayContents);
+      <h4 className="big-mrg-bottom">Reader</h4>
 
-    // Make the webview transparent so the video preview is visible behind it.
-    QRScanner.show();
-  }
-  render() {
-    return (
-      <div className="home-page">
-        <div className="center-block">
-          <h2>QR</h2>
+      <FlatButton
+        backgroundColor="#7D7D7D"
+        hoverColor="#7D7D7D"
+      >
+        <Link to="/scanner">
+          <div className="icon qr-icon"></div>
+        </Link>
+      </FlatButton>
 
-          <h4 className="big-mrg-bottom">Reader</h4>
-
-          <FlatButton
-            backgroundColor="#7D7D7D"
-            hoverColor="#7D7D7D"
-          >
-            <Link to="/scanner">
-              <div className="icon qr-icon"></div>
-            </Link>
-          </FlatButton>
-
-        </div>
-      </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 export default Home;
